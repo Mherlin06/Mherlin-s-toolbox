cc8d519cc30106c8ef3403735d23477894fb0ceb
Hugues Mourice <mourice.hugues@gmail.com>
2022-05-02 20:49:08 +0200

    faq section rebuilding

--- app.js/faq.js
+++ app.js/faq.js
@@ -1,7 +1,7 @@
 const plusBtn = document.querySelectorAll('.faq-btn-plus');
 const minusBtn = document.querySelectorAll('.faq-btn-minus');
 
-plusBtn.forEach(button => { button.addEventListener('click', function(e){
+plusBtn.forEach(button => { button.addEventListener('click', (e) => {
     e.currentTarget.parentElement.nextSiblingElement.classList.toggle('show-answer');
     });
     
